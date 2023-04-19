bde1384dbb08400f6757c8d175ae7eacba541909
DHRUVOPS360 <128677270+DHRUVOPS360@users.noreply.github.com>
2023-04-19 20:07:35 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,41 +1,12 @@
-resource "google_app_engine_application" "app" {
-  project     = "engineer-cloud-nprod"
-  location_id = "us-central1"
-
-  # The following settings are required for the Hello World app
-  database_type = "CLOUD_DATASTORE_COMPATIBILITY"
-  feature_settings {
-    split_health_checks = false
-  }
-}
-
-resource "google_app_engine_standard_app_version" "version" {
-  project = "engineer-cloud-nprod"
-  service = "default"
-  runtime = "nodejs14"
-
-  # The following settings are required for the Hello World app
-  entrypoint = "node app.js"
-  deployment {
-    files {
-      "app.yaml" = <<-YAML
-        runtime: nodejs14
-        handlers:
-          - url: /.*
-            script: auto
-        YAML
-      "app.js" = <<-JS
-        const http = require('http');
-        const port = process.env.PORT || 8080;
-        const server = http.createServer((req, res) => {
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'text/plain');
-          res.end('Hello, World!');
-        });
-        server.listen(port, () => {
-          console.log(`Server running on port ${port}`);
-        });
-        JS
+resource "google_app_engine_application" "main" {
+  project        = var.project_id
+  location_id    = var.location_id
+  auth_domain    = var.auth_domain
+  serving_status = var.serving_status
+  dynamic "feature_settings" {
+    for_each = var.feature_settings
+    content {
+      split_health_checks = lookup(feature_settings.value, "split_health_checks", true)
     }
   }
 }
