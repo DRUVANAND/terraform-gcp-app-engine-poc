27d0456486bfda20ffa93ed61d28dfcf289b6ade
DHRUVOPS360 <128677270+DHRUVOPS360@users.noreply.github.com>
2023-04-20 13:31:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,45 +47,9 @@ resource "google_app_engine_standard_app_version" "default_v1" {
   delete_service_on_destroy = true
   service_account = google_service_account.custom_service_account.email
 }
-/.resource "google_app_engine_standard_app_version" "myapp_v1" {
-  version_id = "v1"
-  service    = "myapp"
-  runtime    = "nodejs10"
 
-  entrypoint {
-    shell = "node ./app.js"
-  }
 
-  deployment {
-    zip {
-      source_url = "https://storage.googleapis.com/${google_storage_bucket.bucket.name}/${google_storage_bucket_object.object.name}"
-    }
-  }
 
-  env_variables = {
-    port = "8080"
-  }
- 
-
-  automatic_scaling {
-    max_concurrent_requests = 10
-    min_idle_instances = 1
-    max_idle_instances = 3
-    min_pending_latency = "1s"
-    max_pending_latency = "5s"
-    standard_scheduler_settings {
-      target_cpu_utilization = 0.5
-      target_throughput_utilization = 0.75
-      min_instances = 2
-      max_instances = 10
-    }
-  }
-
-  delete_service_on_destroy = true
-  service_account = google_service_account.custom_service_account.email
-}
-./
-  
 resource "google_storage_bucket" "bucket" {
   name     = "dhruv-appengine-github"
   location = "US"
