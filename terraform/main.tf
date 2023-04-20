c9f3e8c475fe9b054f67508e696f66d4f275d13b
DHRUVOPS360 <128677270+DHRUVOPS360@users.noreply.github.com>
2023-04-20 12:09:34 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,41 @@ resource "google_app_engine_application" "main" {
     }
   }
 }
+
+resource "google_app_engine_standard_app_version" "app_v1" {
+  version_id = "v1"
+  service    = "default"
+  runtime    = "go115"
+ 
+  entrypoint {
+    shell = "main"
+  }
+ 
+  deployment {
+    zip {
+      source_url = "https://storage.googleapis.com/${google_storage_bucket.app.name}/${google_storage_bucket_object.app.name}"
+    }
+  }
+ 
+ 
+  automatic_scaling {
+    max_concurrent_requests = 10
+    min_idle_instances      = 1
+    max_idle_instances      = 3
+    min_pending_latency     = "1s"
+    max_pending_latency     = "5s"
+ 
+    standard_scheduler_settings {
+      target_cpu_utilization        = 0.5
+      target_throughput_utilization = 0.75
+      min_instances                 = var.min_instances
+      max_instances                 = var.max_instances
+    }
+  }
+ 
+  vpc_access_connector {
+    name = google_vpc_access_connector.connector.self_link
+  }
+ 
+  delete_service_on_destroy = true
+}
